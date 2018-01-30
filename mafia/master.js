484ef566b82390ec297424a05e56d575e9c6f13a
Krytic <Krytic@users.noreply.github.com>
2018-01-30 16:16:57 +1300

    adds polish

--- mafia/master.js
+++ mafia/master.js
@@ -1,6 +1,31 @@
 $(document).ready(function(){
 	player_list = {};
 
+	function win(alignment) {
+		mode = (alignment == "Mafia" ? "danger": "success")
+		show_message(mode, "Winner!", alignment + " have won the game!");
+	}
+
+	function remove_player(name) {
+		role = player_list[name];
+
+		$("[data-player=" + name + "]").slideUp(1000);
+
+		if (role == "mafia") {
+			mafioso--;
+		}
+		else {
+			villagers--;
+		}
+
+		if (mafioso == villagers) {
+			win("Mafia");
+		}
+		else if (mafioso == 0) {
+			win("Town");
+		}
+	}
+
 	function shuffle(array) {
 		var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -20,21 +45,17 @@ $(document).ready(function(){
 	function add_player(name) {
 		$("#no-players").remove();
 
-		$("#playerlist").append('<div class="panel-block" data-player="' + name + '">' + name + '<a class="is-pulled-right" href="#" id="kill" data-to-kill="' + name + '">lynch / kill</a></div>');
+		$("#playerlist").append('<div class="panel-block" data-player="' + name + '">' + name + '<a class="is-pulled-right" data-kill="' + name + '">lynch / kill</a></div>');
 
 		$('[data-player="' + name + '"]').slideDown(1000);
 
 		player_list[name] = "";
 	}
 
-	function remove_player() {
-		return;
-	}
-
 	function show_message(type, title, body) {
 		$("#messages").html(`<article class="message is-` + type + `">
 			<div class="message-header">
-			<p>` + title + `</p>
+			<p><strong>` + title + `</strong></p>
 			</div>
 			<div class="message-body">
 			` + body + `
@@ -45,6 +66,11 @@ $(document).ready(function(){
 	add_player("sean");
 	add_player("john");
 	add_player("dave");
+	add_player("ryan");
+
+	$("[data-kill]").on("click", function(){
+		remove_player($(this).data("kill"));
+	})
 
 	$('#new-player').on('keypress', function (e) {
 		if(e.which === 13){
@@ -69,6 +95,7 @@ $(document).ready(function(){
 			nurse = 1;
 			sheriff = 1;
 			town = players - mafioso - nurse - sheriff;
+			villagers = players - mafioso
 
 			roles = [];
 
@@ -88,6 +115,8 @@ $(document).ready(function(){
 				roles.push("town");
 			}
 
+			shuffle(roles);
+
 			show_message("info", "The Honour Code", "Mafia runs on the honour code. Please only scan the QR code below if your name is shown beneath it.");
 
 			i = 0
@@ -96,31 +125,35 @@ $(document).ready(function(){
 				i++;
 			}
 
+			console.log(player_list);
+
 			$("#begin-game").css({"display": "none"});
 			$("#next-player").css({"visibility": "visible"});
 		} else {
-			console.log("error");
 			show_message("danger", "Error - Not enough players", "Mafia requires at least <strong>4</strong> players to run. You are trying to play with <strong>" + players + "</strong>. Please add some more players and try again.");
-			add_player("ryan");
 		}
 	});
 
 	$("#next-player").on("click",function(e){
+		if (showing_player == players) {
+			$("#next-player").css({"display": "none"});
+			$("#active-qrcode").css({"display": "none"});
+			$("#current-player-shown").css({"display": "none"});
+			show_message("primary", "The game is now running!", "Good Luck and Lynch Well!")
+		}
+
 		e.preventDefault();
 		name = Object.keys(player_list)[showing_player];
 		role = player_list[name];
 
-		console.log(name + " is " + role);
-
 		pathname = window.location.pathname.split("/");
 		pathname.pop();
 		pathname = pathname.join("/");
-		console.log(pathname);
 
 		url = window.location.hostname + pathname + "/role.html?name=" + name + "&role=" + role;
-		$("#active-qrcode").val("");
-		$("#active-qrcode").qrcode(url);
-		$("#current-player-shown").val(name);
+		$("#active-qrcode").empty().qrcode(url);
+
+		document.getElementById('current-player-shown').innerHTML = name;
 
 		showing_player++;
 	});
