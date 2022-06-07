9f1828f73caaa71034f13af1cef7649e2f63023b
Krytic <krytical.programmer@gmail.com>
2022-06-07 22:42:48 +1200

    add first code overlay

--- js/scripts.js
+++ js/scripts.js
@@ -10,5 +10,18 @@ jQuery(function ($) {
         $('#preloader').delay(200).fadeOut('slow');
     }());
 
+    $(document).ready(function(){
+		var url = 'https://cdn.jsdelivr.net/gh/krytic/kaitiaki/kaitiaki/plotfile.py';
+
+		$.get(url, function(data) {
+			$('#kaitiaki-codeblock').text(data);
+			hljs.highlightAll();
+		}, 'text');
+
+
+	    $("#show-kaitiaki").on('click', function(){
+	    	$("#kaitiaki-code").show("slide", { direction: "right" }, 2000);
+	    });
+	});
 
 }); // JQuery end
