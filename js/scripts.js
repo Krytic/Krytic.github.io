8db9972a04fd5e305d73426fc1c4d0defb90e293
Sean Richards <Krytic@users.noreply.github.com>
2022-12-28 22:48:30 +1300

    major tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -10,23 +10,35 @@ jQuery(function ($) {
         $('#preloader').delay(200).fadeOut('slow');
     }());
 
-    $(document).ready(function(){
-		var url = 'https://cdn.jsdelivr.net/gh/krytic/kaitiaki/kaitiaki/plotfile.py';
+    function setup_codeblock(codename, filestring) {
+		var url = 'https://cdn.jsdelivr.net/gh/krytic/' + codename + "/" + filestring;
 
 		$.get(url, function(data) {
-			$('#kaitiaki-codeblock').text(data);
-			hljs.highlightAll();
+			$('#' + codename + '-codeblock').text(data);
 		}, 'text');
 
 
-	    $("#show-kaitiaki").on('click', function(){
-	    	$("#kaitiaki-code").show("slide", { direction: "right" }, 2000);
+	    $("#show-" + codename).on('click', function(){
+		    hljs.highlightElement(document.getElementById(codename + "-codeblock"));
+	    	$("#" + codename + "-code").show("slide", { direction: "right" }, 2000);
 	    	// $("#dismiss").show("slide", { direction: "right" }, 2000);
 	    });
 
-	    $("#dismiss-btn").on('click', function(){
-	    	$("#kaitiaki-code").hide("slide", { direction: "right" }, 2000);
+	    $("#dismiss-" + codename).on('click', function(){
+	    	$('#' + codename + "-code").hide("slide", { direction: "right" }, 2000);
 	    });
+
+    }
+
+    $(document).ready(function(){
+    	function run_setups() {
+			setup_codeblock('kaitiaki', 'kaitiaki/plotfile.py');
+			setup_codeblock('takahe', 'src/integrator.jl');
+			setup_codeblock('MyBBWiki', 'Upload/inc/plugins/wiki.php');
+    	}
+
+    	run_setups();
+
 	});
 
 }); // JQuery end
